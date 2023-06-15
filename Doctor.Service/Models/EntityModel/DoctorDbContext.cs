e6f8d0b3aa2e034da08727515916338def8855ad
Khairul Kabir <khairul.kabir@wundermanthompson.com>
2023-06-15 13:47:10 +0600

    Error Free

--- Doctor.Service/Models/EntityModel/DoctorDbContext.cs
+++ Doctor.Service/Models/EntityModel/DoctorDbContext.cs
@@ -19,7 +19,7 @@ public partial class DoctorDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=Kabir;Initial Catalog=DoctorDB;Integrated Security=True;Pooling=False;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=BDWTLKHAIRULKA2\\SQLEXPRESS;Initial Catalog=DoctorDB;Integrated Security=True;Pooling=False;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
